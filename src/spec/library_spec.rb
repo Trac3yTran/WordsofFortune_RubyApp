eff11b23299dd194f74365ef749f9d19b23c347b
Trac3yTran <tran.tracey90@gmail.com>
2020-10-02 13:11:33 +1000

    successfully tested libray_spec

---
+++ src/spec/library_spec.rb
@@ -0,0 +1,20 @@
+
+require 'rspec'
+require_relative '../model/library.rb'
+
+RSpec.describe Library do
+it 'should contain the random word arrays and meanings' do
+    puzzle = Library.words
+    expect(puzzle).to eq [
+        ["sister", "A female sibling"],
+        ["software", "Set of programs, which is designed to perform a well-defined function"],
+        ["continent", "There are 7 of these large landmasses in the world"],
+        ["morning", "The first or early part of the day, lasting from midnight to noon or from sunrise to noon"],
+        ["galaxy", "Is a huge collection of gas, dust, and billions of stars and their solar systems"]
+]
+
+end
+end
+
+
+
